5f47731d9f5fffc7d86f3321fbadf4fad3a8a586
Jason Vance <jbvance@gmail.com>
2017-12-25 12:46:49 -0600

    complete lab

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,7 +16,12 @@ class Game extends React.Component {
     };
   }
 
+
   // TODO: create a componentWillMount() which will set the current time
+
+  componentWillMount() {
+    this.setCurrentTime();
+  }
 
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
